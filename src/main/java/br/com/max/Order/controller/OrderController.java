615a97d92969162b372011ae77771a11077b7808
Maxsuel Santos <santosmaxsuel17@gmail.com>
2021-04-22 14:37:22 -0300

    Developed get order by id method

--- src/main/java/br/com/max/Order/controller/OrderController.java
+++ src/main/java/br/com/max/Order/controller/OrderController.java
@@ -2,7 +2,11 @@ package br.com.max.Order.controller;
 
 import br.com.max.Order.entity.Order;
 import br.com.max.Order.service.OrderService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,16 +14,18 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/orders")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class OrderController {
 
     private OrderService orderService;
 
-    public OrderController(OrderService orderService) {
-        this.orderService = orderService;
-    }
-
     @GetMapping
     public List<Order> findAll() {
        return orderService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Order> findById(@PathVariable Long id) {
+        return orderService.findById(id);
     }
 }
--- src/main/java/br/com/max/Order/service/OrderService.java
+++ src/main/java/br/com/max/Order/service/OrderService.java
@@ -2,20 +2,29 @@ package br.com.max.Order.service;
 
 import br.com.max.Order.entity.Order;
 import br.com.max.Order.repository.OrderRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class OrderService {
 
     private OrderRepository orderRepository;
 
-    public OrderService(OrderRepository orderRepository) {
-        this.orderRepository = orderRepository;
-    }
-
     public List<Order> findAll() {
         return orderRepository.findAll();
+    }
+
+    public ResponseEntity<Order> findById(Long id) {
+        Optional<Order> optionalOrder = orderRepository.findById(id);
+        if (optionalOrder.isPresent()) {
+            return ResponseEntity.ok(optionalOrder.get());
+        }
+        return ResponseEntity.notFound().build();
     }
 }
